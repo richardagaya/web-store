dc419026c4afa9ee2adcb3582f3ac39ce328415f
richard agaya <richardagaya278@gmail.com>
2024-06-25 00:34:54 +0300

    new update

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -3,8 +3,8 @@ import React from 'react';
 
 const Hero: React.FC = () => {
   return (
-    <section className="bg-hero-pattern bg-cover bg-center bg-black text-white h-screen flex items-center justify-center font-Lato">
-      <div className="text-center">
+    <section className="relative h-screen bg-black text-white flex items-center justify-center font-Lato">
+      <div className="absolute z-10 text-center">
         <h1 className="text-4xl md:text-6xl font-bold">Welcome to Jukoro learning</h1>
         <p className="flex flex-col mt-4 text-lg md:text-2xl">
           Get access to over <span className="text-green-600 text-3xl">200+ e-books and tutorials</span>
@@ -13,5 +13,4 @@ const Hero: React.FC = () => {
     </section>
   );
 };
-
 export default Hero;
